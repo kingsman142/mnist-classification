ec0e1a74a3ae4df3dabbf124af5a81097e2fa2e4
kingsman142 <jamhahn@yahoo.com>
2019-12-23 21:13:24 -0500

    Added comments and removed one redundant line of code in train.py

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import torchvision.transforms as transforms
 from model import MNISTClassifier
 from utils import *
 
+# use GPU if available, otherwise use the CPU
 print("GPU available: {}".format(torch.cuda.is_available()))
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -16,7 +17,7 @@ model = MNISTClassifier(pretrained = True).to(device)
 
 transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,)),
+    transforms.Normalize((0.5,), (0.5,)), # normalize images
 ])
 
 # set up train and test datasets
@@ -25,8 +26,6 @@ mnist_test = datasets.MNIST("data", train = False, transform = transform, target
 
 print("Train size: {}".format(len(mnist_train)))
 print("Test size: {}".format(len(mnist_test)))
-
-img, label = mnist_train[0]
 
 # set up train and test dataset loaders
 train_loader = torch.utils.data.DataLoader(mnist_train, batch_size = args.batch_size, shuffle = True)
@@ -47,32 +46,40 @@ print("\tBatch size: {}".format(args.batch_size))
 # train the model
 for epoch in range(args.num_epochs):
     for batch_id, sample in enumerate(train_loader):
+        # get the data (images and labels)
         imgs, labels = sample
         imgs = imgs.to(device)
         true_labels = labels.to(device)
 
+        # make predictions and calculate loss
         pred_scores = model(imgs).to(device)
         loss = loss_func(pred_scores, true_labels)
 
+        # weight update
         model.zero_grad()
         loss.backward()
         optimizer.step()
 
+        # log info for the user
         if batch_id % 10 == 0:
             print("(train) => Epoch {}/{} - Batch {}/{} - Loss: {}".format(epoch, args.num_epochs, batch_id, len(train_loader), loss.item()))
 
 # test the model
 correct = 0
 for batch_id, sample in enumerate(test_loader):
+    # get the data (images and labels)
     imgs, labels = sample
     imgs = imgs.to(device)
     true_labels = labels.to(device)
 
+    # make predictions for each class
     pred_scores = model(imgs).to(device)
 
-    pred_label = torch.argmax(pred_scores, 1)
-    correct += torch.sum(pred_label == true_labels).item()
+    # take the highest class prediction and count how many labels matched the ground-truth labels
+    pred_label = torch.argmax(pred_scores, 1) # i.e. if predictions were [0.1, 0.0, 0.6, ..., 0.0], it'd return index/class 2
+    correct += torch.sum(pred_label == true_labels).item() # count how many were correct
 
+    # log info for the user
     if batch_id % 100 == 0:
         print("(test) => Batch {}/{}".format(batch_id, len(test_loader)))
 test_accuracy = correct / len(mnist_test)
